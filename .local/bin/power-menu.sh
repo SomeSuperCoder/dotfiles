36e5f04a30a7175bf8ef239001b2e2c76167b73d
SomeSuperCoder <ithelper9@gmail.com>
2025-07-24 14:19:40 +0300

    Keyboard layout switch bug fixed

--- .local/bin/power-menu.sh
+++ .local/bin/power-menu.sh
@@ -5,6 +5,6 @@ chosen=$(printf "Power Off\nRestart\nLock" | rofi -dmenu -i)
 case "$chosen" in
     "Power Off") poweroff ;;
     "Restart") reboot ;;
-    "Lock") hyprctl switchxkblayout at-translated-set-2-keyboard 0 & hyprlock ;;
+    "Lock") hyprctl switchxkblayout all 0 & hyprlock ;;
     *) exit 1 ;;
 esac
